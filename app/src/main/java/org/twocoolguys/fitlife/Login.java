031a4f88d946f267858ceacb604ce0dcd6a16417
7chris71000 <c.francis7@live.ca>
2018-06-02 13:53:56 -0400

    Additions

--- app/src/main/java/org/twocoolguys/fitlife/Login.java
+++ app/src/main/java/org/twocoolguys/fitlife/Login.java
@@ -16,37 +16,37 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void enterOnClick(View view){
-        UserDatabase users = new UserDatabase(this);
-
-        EditText textName = (EditText)findViewById(R.id.usernameLogin);
-        EditText textPassword = (EditText)findViewById(R.id.passwordLogin);
-        TextView textError = (TextView)findViewById(R.id.confirmError);
-
-        String name = textName.getText().toString();
-        String password = textPassword.getText().toString();
-
-
-        if(users.authUser(name, password)){
-
-            SharedPreferences.Editor editor = getSharedPreferences("sessionDetails", MODE_PRIVATE).edit();
-            editor.putString("sessionUsername", name);
-            editor.apply();
-
-            Intent intent = new Intent(getBaseContext(), Homepage.class);
-            startActivity(intent);
-
-        } else if(users.checkUser(name)){
-            textError.setText("Incorrect password");
-            textError.setVisibility(View.VISIBLE);
-        }
-        else {
-            textError.setText("Unrecognized user");
-            textError.setVisibility(View.VISIBLE);
-        }
-
-
-
-    }
+//    public void enterOnClick(View view){
+//        UserDatabase users = new UserDatabase(this);
+//
+//        EditText textName = (EditText)findViewById(R.id.usernameLogin);
+//        EditText textPassword = (EditText)findViewById(R.id.passwordLogin);
+//        //TextView textError = (TextView)findViewById(R.id.confirmError);
+//
+//        String name = textName.getText().toString();
+//        String password = textPassword.getText().toString();
+//
+//
+//        if(users.authUser(name, password)){
+//
+//            SharedPreferences.Editor editor = getSharedPreferences("sessionDetails", MODE_PRIVATE).edit();
+//            editor.putString("sessionUsername", name);
+//            editor.apply();
+//
+//            Intent intent = new Intent(getBaseContext(), Homepage.class);
+//            startActivity(intent);
+//
+//        } else if(users.checkUser(name)){
+//            textError.setText("Incorrect password");
+//            textError.setVisibility(View.VISIBLE);
+//        }
+//        else {
+//            textError.setText("Unrecognized user");
+//            textError.setVisibility(View.VISIBLE);
+//        }
+//
+//
+//
+//    }
 
 }
