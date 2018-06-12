45033d26d9802938bfb50a438d95567669d2c97f
Ebrahim Khan <ebropower@gmail.com>
2018-06-12 18:30:07 -0400

    routines screen and the muscle pages

---
+++ app/src/main/java/org/twocoolguys/fitlife/Arms.java
@@ -0,0 +1,13 @@
+package org.twocoolguys.fitlife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Arms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arms);
+    }
+}
--- app/src/main/java/org/twocoolguys/fitlife/Routine.java
+++ app/src/main/java/org/twocoolguys/fitlife/Routine.java
@@ -1,13 +1,84 @@
 package org.twocoolguys.fitlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Routine extends AppCompatActivity {
+
+    ImageView chest, back, legs, arms, core;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routine);
+
+        chest = (ImageView) findViewById(R.id.chestImage);
+        back = (ImageView) findViewById(R.id.backImage);
+        legs = (ImageView) findViewById(R.id.legsImage);
+        arms = (ImageView) findViewById(R.id.armsImage);
+        core = (ImageView) findViewById(R.id.coreImage);
+
+        chest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Routine.this, Chest.class);
+                startActivity(i);
+
+            }
+
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Routine.this, Back.class);
+                startActivity(i);
+
+            }
+
+        });
+
+        legs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Routine.this, Legs.class);
+                startActivity(i);
+
+            }
+
+        });
+
+        arms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Routine.this, Arms.class);
+                startActivity(i);
+
+            }
+
+        });
+
+        core.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Routine.this, Core.class);
+                startActivity(i);
+
+            }
+
+        });
+
+
+
+
     }
 }
